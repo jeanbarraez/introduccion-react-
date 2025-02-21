2027ba465e7eaeac48a0e44e2e0e7915d18b0e0c
jean barraez <jeanbarraez@users.noreply.github.com>
2025-02-20 22:47:27 -0400

    Se realizo cambios en el css Adoptar y cambios en el componente Tags , cambiando buuton pot tags de react boostrap

--- Primer-Desafio-react/src/components/Tags.jsx
+++ Primer-Desafio-react/src/components/Tags.jsx
@@ -1,9 +1,9 @@
-import Button from "react-bootstrap/Button";
-
+//import Button from "react-bootstrap/Button";
+import Badge from 'react-bootstrap/Badge';
 const Tags = ({colorButton, raza}) => {
   return (
     <>
-      <Button variant={colorButton}>{raza}</Button>
+      <Badge bg={colorButton} className='boton'>{raza}</Badge>
     </>
   );
 };
